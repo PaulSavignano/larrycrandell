29daff0239882bbb385fef5138bfe55f61fc4d3f
Paul Savignano <Paul.Savignano@gmail.com>
2017-08-10 12:23:13 -0700

    Add scrollIntoView() for hash links resulting from search

--- client/src/containers/pages/Page.js
+++ client/src/containers/pages/Page.js
@@ -5,7 +5,12 @@ import SectionList from '../../containers/sections/SectionList'
 
 class Page extends Component {
   componentWillMount() {
-    window.scrollTo(0,0)
+    if (window.location.hash) {
+      const target = document.getElementById(window.location.hash)
+      if (target) target.scrollIntoView()
+    } else {
+      window.scrollTo(0,0)
+    }
   }
   render() {
     const { sections } = this.props
