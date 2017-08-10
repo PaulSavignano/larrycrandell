fff02676e1f71d69ae1bc5eb0d5d039f1ac528f8
Paul Savignano <Paul.Savignano@gmail.com>
2017-08-10 16:31:50 -0700

    Update from master

--- client/src/containers/pages/Page.js
+++ client/src/containers/pages/Page.js
@@ -4,13 +4,24 @@ import pageContainer from './pageContainer'
 import SectionList from '../../containers/sections/SectionList'
 
 class Page extends Component {
-  componentWillMount() {
-    if (window.location.hash) {
-      const target = document.getElementById(window.location.hash)
-      if (target) target.scrollIntoView()
+  state = {
+    timeoutId: null
+  }
+  componentDidMount() {
+    const { hash } = window.location;
+    if (hash !== '') {
+      const timeoutId = setTimeout(() => {
+        const id = hash.replace('#', '');
+        const element = document.getElementById(id);
+        if (element) element.scrollIntoView();
+      }, 300);
+      this.setState({ timeoutId })
     } else {
-      window.scrollTo(0,0)
+      window.scrollTo(0, 0)
     }
+  }
+  componentWillUnmount() {
+    clearTimeout(this.state.timeoutId)
   }
   render() {
     const { sections } = this.props
